6f65f5224a84dcd04847786a16c0132c5e928b40
Engin Arslan <hibernationtheory@gmail.com>
2023-01-21 10:42:50 +0100

    initial commit for 02

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,8 @@
-import Link from "next/link";
-
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <header className="py-3 flex justify-center">
-        <div className="flex items-center justify-between w-full max-w-2xl">
-          <Link href="/">
-            <a className={`font-bold text-indigo-600 hover:text-indigo-700`}>
-              Home
-            </a>
-          </Link>
-
-          <Link href="/withdraw">
-            <a className={`font-bold text-indigo-600 hover:text-indigo-700`}>
-              Withdraw
-            </a>
-          </Link>
-        </div>
-      </header>
       <Component {...pageProps} />
     </>
   );
