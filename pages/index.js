d97c112390b5846bf5220ab77c399966718790bd
Engin Arslan <hibernationtheory@gmail.com>
2023-01-21 11:14:22 +0100

    initial commit for 07

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,90 @@ function MainButton({ onClick, disabled, label }) {
   )
 }
 
+function AddressInput({ id, label, value, onChange, onBlur, error }) {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={`address-${id}`}>{label}</label>
+      <input
+        id={`address-${id}`}
+        value={value}
+        type="text"
+        onChange={(event) => onChange(event.target.value)}
+        onBlur={(event) => onBlur(event.target.value)}
+        className={'form-input rounded px-4 py-3'}
+        placeholder="Wallet Address"
+      />
+      {error ? (
+        <p className="text-red-600 text-sm">{error}</p>
+      ) : (
+        <p className="text-sm">&nbsp;</p>
+      )}
+    </div>
+  )
+}
+
+function SplitInput({ id, value, onChange }) {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={`split-${id}`}>Split</label>
+      <input
+        id={`split-${id}`}
+        value={value}
+        type="number"
+        onChange={(event) => onChange(Number(event.target.value))}
+        className={'form-input rounded px-4 py-3'}
+        placeholder={1}
+        min={1}
+        step={1}
+      />
+    </div>
+  )
+}
+
+function PartnerInput({ address, split }) {
+  return (
+    <div className={'flex'}>
+      <div className={'mr-3 w-full'}>
+        <AddressInput
+          id={address.id}
+          label={address.label}
+          value={address.value}
+          onChange={address.onChange}
+          onBlur={address.onBlur}
+          error={address.error}
+        />
+      </div>
+      <div className={'w-1/5'}>
+        <SplitInput
+          id={split.id}
+          value={split.value}
+          onChange={split.onChange}
+        />
+      </div>
+    </div>
+  )
+}
+
 export default function Home() {
   const web3 = useRef(null)
   const [currentAccount, setCurrentAccount] = useState(null)
   const [hasWalletWarning, setHasWalletWarning] = useState(false)
+  const [partners, setPartners] = useState([
+    {
+      id: '1',
+      label: 'Partner A',
+      address: '',
+      error: '',
+      split: 1,
+    },
+    {
+      id: '2',
+      label: 'Partner B',
+      address: '',
+      error: '',
+      split: 1,
+    },
+  ])
 
   const checkIfWalletIsConnected = () => {
     return Boolean(window.ethereum)
@@ -63,6 +143,51 @@ export default function Home() {
     }
   }
 
+  const addressInputs = partners.map((partner, index) => {
+    return (
+      <PartnerInput
+        address={{
+          id: partner.id,
+          label: partner.label,
+          value: partner.address,
+          onChange: (value) => {
+            setPartners((oldPartnersState) => {
+              const newPartnersState = [...oldPartnersState]
+              newPartnersState[index].address = value
+
+              return newPartnersState
+            })
+          },
+          onBlur: (value) => {
+            setPartners((oldPartnersState) => {
+              const isValueAddress = web3.current.utils.isAddress(value)
+              const newPartnersState = [...oldPartnersState]
+              newPartnersState[index].error = isValueAddress
+                ? ''
+                : 'Enter a valid wallet address'
+
+              return newPartnersState
+            })
+          },
+          error: partner.error,
+        }}
+        split={{
+          id: partner.id,
+          value: partner.split,
+          onChange: (value) => {
+            setPartners((oldPartnersState) => {
+              const newPartnersState = [...oldPartnersState]
+              newPartnersState[index].split = value
+
+              return newPartnersState
+            })
+          },
+        }}
+        key={partner.label}
+      />
+    )
+  })
+
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex flex-1 flex-col items-center justify-start py-8 pt-12  px-6 md:pt-20 text-zinc-700">
@@ -86,6 +211,10 @@ export default function Home() {
             </div>
           )}
         </section>
+
+        {currentAccount && (
+          <div className="grid grid-cols-1 gap-3 mb-6">{addressInputs}</div>
+        )}
       </main>
     </div>
   )
