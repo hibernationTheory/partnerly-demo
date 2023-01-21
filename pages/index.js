fe8757630f1ed63a1a600a082d0535b440c1791c
Engin Arslan <hibernationtheory@gmail.com>
2023-01-21 10:57:08 +0100

    initial commit for 05

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,16 @@ import { useEffect, useRef, useState } from 'react'
 
 function MainButton({ onClick, disabled, label }) {
   return (
-    <button onClick={onClick} disabled={disabled}>
-      <span>{label}</span>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`inline-flex rounded-md shadow disabled:opacity-50`}
+    >
+      <span
+        className={`inline-flex items-center justify-center px-5 py-3 border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700`}
+      >
+        {label}
+      </span>
     </button>
   )
 }
@@ -56,16 +64,28 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <main>
-        {hasWalletWarning && (
-          <p>You will need MetaMask or equivalent to use this app.</p>
-        )}
-        {!currentAccount && (
-          <div>
-            <MainButton onClick={connectWallet} label={'Connect Wallet'} />
-          </div>
-        )}
+    <div className="flex flex-col min-h-screen">
+      <main className="flex flex-1 flex-col items-center justify-start py-8 pt-12  px-6 md:pt-20 text-zinc-700">
+        <h1 className="text-7xl md:text-6xl font-extrabold text-indigo-600 mb-3 pb-2">
+          Partnerly
+        </h1>
+        <section className="max-w-md text-center mb-12">
+          <p className="text-xl">
+            Partnerly creates a smart contract for you and your partner that
+            distributes the payments to the partnership contract in a
+            predetermined split ratio.
+          </p>
+          {hasWalletWarning && (
+            <p className="mt-4 text-red-600">
+              You will need Metamask or equivalent to use this app.
+            </p>
+          )}
+          {!currentAccount && (
+            <div className="mt-4">
+              <MainButton onClick={connectWallet} label={'Connect Wallet'} />
+            </div>
+          )}
+        </section>
       </main>
     </div>
   )
