8880be941c37a2e2ce8186affc88fb04a6cfd75e
Engin Arslan <hibernationtheory@gmail.com>
2023-01-21 10:47:58 +0100

    initial commit for 03

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
-import { useEffect, useState } from 'react'
+import Web3 from 'web3'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
+  const web3 = useRef(null)
   const [hasWalletWarning, setHasWalletWarning] = useState(false)
 
   const checkIfWalletIsConnected = () => {
     return Boolean(window.ethereum)
   }
-  //New addition
+
   useEffect(() => {
     const hasWallet = checkIfWalletIsConnected()
     setHasWalletWarning(!hasWallet)
+  }, [])
+
+  useEffect(() => {
+    if (web3.current) {
+      return
+    }
+
+    if (!checkIfWalletIsConnected()) {
+      return
+    }
+
+    web3.current = new Web3(window.ethereum)
+    web3.current.eth.getBlock('latest').then((block) => console.log(block))
   }, [])
 
   return (
