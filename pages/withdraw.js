afea923743eeed82177146ca4892e972db7603dd
Engin Arslan <hibernationtheory@gmail.com>
2023-01-21 11:36:16 +0100

    initial commit for 09

--- pages/withdraw.js
+++ pages/withdraw.js
@@ -1,92 +1,94 @@
-import { useEffect, useRef, useState } from "react";
-import Web3 from "web3";
+import { useEffect, useRef, useState } from 'react'
+import Web3 from 'web3'
 
-import Contract from "../contract/Contract";
-import { MainButton, AddressInput } from "../components";
+import Contract from '../contract/Contract'
+import { MainButton, AddressInput } from '../components'
 
 export default function Withdraw() {
-  const web3 = useRef(null);
+  const web3 = useRef(null)
 
-  const [address, setAddress] = useState("");
-  const [error, setError] = useState(false);
-  const [isProcessing, setIsProcessing] = useState(false);
-  const [currentAccount, setCurrentAccount] = useState(null);
-  const [hasWalletWarning, setHasWalletWarning] = useState(false);
+  const [address, setAddress] = useState('')
+  const [error, setError] = useState(false)
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [currentAccount, setCurrentAccount] = useState(null)
+  const [hasWalletWarning, setHasWalletWarning] = useState(false)
 
   useEffect(() => {
-    const hasWallet = checkIfWalletIsConnected();
-    setHasWalletWarning(!hasWallet);
-  }, []);
+    const hasWallet = checkIfWalletIsConnected()
+    setHasWalletWarning(!hasWallet)
+  }, [])
 
   useEffect(() => {
     if (web3.current) {
-      return;
+      return
     }
 
     if (!checkIfWalletIsConnected()) {
-      return;
+      return
     }
 
-    web3.current = new Web3(window.ethereum);
-    web3.current.eth.getBlock("latest").then(console.log);
-  }, []);
+    web3.current = new Web3(window.ethereum)
+    web3.current.eth.getBlock('latest').then(console.log)
+  }, [])
 
   useEffect(() => {
-    getAuthorizedAccount();
-  }, []);
+    getAuthorizedAccount()
+  }, [])
 
   const checkIfWalletIsConnected = () => {
-    return Boolean(window.ethereum);
-  };
+    return Boolean(window.ethereum)
+  }
 
   const getAuthorizedAccount = async () => {
     if (!checkIfWalletIsConnected()) {
-      return;
+      return
     }
 
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+      const accounts = await ethereum.request({
+        method: 'eth_accounts',
+      })
 
       if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
+        const account = accounts[0]
+        console.log('Found an authorized account:', account)
+        setCurrentAccount(account)
       } else {
-        console.log("No authorized account found");
+        console.log('No authorized account found')
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const connectWallet = async () => {
     if (!checkIfWalletIsConnected()) {
-      return;
+      return
     }
 
     try {
-      const { ethereum } = window;
+      const { ethereum } = window
 
       const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
+        method: 'eth_requestAccounts',
+      })
 
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
+      console.log('Connected', accounts[0])
+      setCurrentAccount(accounts[0])
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   const handleWithdraw = async () => {
-    const { abi } = Contract;
-    const contract = new web3.current.eth.Contract(abi, address);
+    const { abi } = Contract
+    const contract = new web3.current.eth.Contract(abi, address)
 
-    const gas = await contract.methods.withdraw().estimateGas();
+    const gas = await contract.methods.withdraw().estimateGas()
 
-    setIsProcessing(true);
+    setIsProcessing(true)
 
     contract.methods
       .withdraw()
@@ -94,36 +96,36 @@ export default function Withdraw() {
         from: currentAccount,
         gas,
       })
-      .on("error", (error) => {
-        console.log("error", error);
-        setIsProcessing(false);
+      .on('error', (error) => {
+        console.log('error', error)
+        setIsProcessing(false)
       })
-      .on("receipt", (receipt) => {
+      .on('receipt', (receipt) => {
         // receipt will contain deployed contract address
-        console.log("Receipt", receipt);
+        console.log('Receipt', receipt)
 
-        setIsProcessing(false);
+        setIsProcessing(false)
       })
-      .on("confirmation", (_confirmationNumber, receipt) => {
-        console.log("Confirmed", receipt);
-      });
-  };
+      .on('confirmation', (_confirmationNumber, receipt) => {
+        console.log('Confirmed', receipt)
+      })
+  }
 
   const addressInput = (
     <AddressInput
-      label={""}
-      id={"withdraw"}
+      id={'withdraw'}
+      label={''}
       value={address}
       onChange={(value) => {
-        setAddress(value);
+        setAddress(value)
       }}
       onBlur={(value) => {
-        const isValueAddress = web3.current.utils.isAddress(value);
-        setError(isValueAddress ? "" : "Enter a valid wallet address");
+        const isValueAddress = web3.current.utils.isAddress(value)
+        setError(isValueAddress ? '' : 'Enter a valid wallet address')
       }}
       error={error}
     />
-  );
+  )
 
   return (
     <main className="flex flex-1 flex-col items-center justify-start py-8 pt-12 md:pt-20 text-zinc-700 px-6">
@@ -141,7 +143,7 @@ export default function Withdraw() {
         )}
         {!currentAccount && (
           <div className="mt-4">
-            <MainButton onClick={connectWallet} label={"Connect Wallet"} />
+            <MainButton onClick={connectWallet} label={'Connect Wallet'} />
           </div>
         )}
 
@@ -154,7 +156,7 @@ export default function Withdraw() {
                 <MainButton
                   onClick={handleWithdraw}
                   disabled={Boolean(error) || !Boolean(address) || isProcessing}
-                  label={isProcessing ? "Processing" : "Withdraw"}
+                  label={isProcessing ? 'Processing' : 'Withdraw'}
                 />
               </div>
             </div>
@@ -162,5 +164,5 @@ export default function Withdraw() {
         )}
       </section>
     </main>
-  );
+  )
 }
